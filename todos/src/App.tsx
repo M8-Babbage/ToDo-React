bd43668529c18dc0493f4c6ca75552157fcdbaf6
Edwin J. Páez <edwinjpr11@gmail.com>
2023-04-04 18:01:36 -0500

    feat(todos): create components using interfaces

--- todos/src/App.tsx
+++ todos/src/App.tsx
@@ -1,6 +1,30 @@
-export default function App() {
+// Interfaces
+import { Todo } from "./models/Todo"
 
+// Componenentes
+import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton"
+import { TodoCounter } from "./components/TodoCounter/TodoCounter"
+import { TodoItem } from "./components/TodoItem/TodoItem"
+import { TodoList } from "./components/TodoList/TodoList"
+import { TodoSearch } from "./components/TodoSearch/TodoSearch"
+
+const todos: Todo[] = [
+  { text: 'Aprender React', completed: true },
+  { text: 'Aprender React 2', completed: true },
+  { text: 'Aprender React 3', completed: true },
+]
+
+const App = () => {
   return (
-    <span>Hola</span>
+    <>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (<TodoItem key={todo.text} text={todo.text} />))}
+      </TodoList>
+      <CreateTodoButton />
+    </>
   )
 }
+
+export { App };
