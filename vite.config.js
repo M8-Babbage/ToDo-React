f263c705dcdc39ae442c2914a01d9565988a6a90
Edwin J. Páez <edwinjpr11@gmail.com>
2023-04-12 11:13:39 -0500

    feat: setup deploy using gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages
+  base: "./",
   // Añadir alias en el proyecto
   resolve: {
     alias: {
